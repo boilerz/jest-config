b591c06f7c20ecbee74d5d8f9b2ef74df15fe36c
momsse <1344228+momsse@users.noreply.github.com>
2021-02-26 18:08:03 +0100

    :bug: fix react-native config

--- src/react-native.ts
+++ src/react-native.ts
@@ -1,10 +1,11 @@
 import type { Config } from '@jest/types';
+import omit from 'lodash.omit';
 
 import base from './base';
 
 const config: Config.InitialOptions = {
   preset: 'react-native',
-  ...base,
+  ...omit(base, 'transform'),
 };
 
 export = config;
